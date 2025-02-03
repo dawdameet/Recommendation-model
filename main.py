d69e975362d7c917a377782ebdfaab400b48ae27
Meet bhai <dawdameet6338@gmail.com>
2025-02-03 15:32:40 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+'''
+    FEATURES: F(i)=[price,category,brand]
+    LIKED ITEMS: L
+    DISLIKED ITEMS: D
+    USER PROFILE: U = (sum(F(I):LIKED)/number of liked items) - (sum(F(i):DISLIKED)/number of disliked items)
+
+'''
